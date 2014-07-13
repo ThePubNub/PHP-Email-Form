74d25ccc5bbd0d76b87583a4904ebe8cf610e9d8
ThePubNub <thepubnub@gmail.com>
2014-07-13 12:19:37 +1200

    mail.php
    This is the file you will go to and send the emails from

---
+++ mail.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pub Nubs Mailer</title>
+</head>
+<body>
+<table border="1">
+<form method='post' action='mailform.php'>
+<tr style="text-align:center">
+	<td colspan="2"><h1>Pub Nubs Emailer</h1></td>
+</tr>
+<tr>
+	<td style="width:200px">From Name:</td>
+    <td style="width:500px"><input type="text" name="fromName" id="fromName" size="80"/></td>
+</tr>
+<tr>
+	<td style="width:200px">From Email:</td>
+    <td style="width:500px"><input type="text" name="fromEmail" id="fromEmail" size="80"/></td>
+</tr>
+<tr>
+	<td style="width:200px">To:</td>
+    <td style="width:500px"><input type="text" name="to" id="to" size="80" /></td>
+</tr>
+<tr>
+	<td style="width:200px">Subject:</td>
+    <td style="width:500px"><input type="text" name="subject" id="subject" size="80" /></td>
+</tr>
+<tr>
+	<td style="width:200px;height:300px">Message:</td>
+    <td style="width:200px"><textarea name="message" id="message" rows="19" cols="80"></textarea></td> 
+</tr>
+<tr>
+	<td colspan="2"><input type="submit" name="submit" id="submit" /></td>
+</tr>
+</form>
+</table>
+</body>
+</html>
